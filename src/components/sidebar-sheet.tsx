70630dba3cf115d83c6f7413e96d60ee0f17a41f
Estevangomez <gomes.estevan1@gmail.com>
2026-02-12 11:16:46 -0400

    feat: setup inicial login com goole / tables prisma / nextauth

--- src/components/sidebar-sheet.tsx
+++ src/components/sidebar-sheet.tsx
@@ -1,30 +1,60 @@
 
 import { Button } from "./ui/button";
 import { SheetContent } from "./ui/sheet";
-import { Avatar, AvatarImage } from "./ui/avatar";
+
 import Image from "next/image";
 import { SheetClose } from "./ui/sheet";
 import { SheetHeader, SheetTitle } from "./ui/sheet";
-import { HomeIcon, Calendar1Icon, LogOutIcon } from "lucide-react";
+import { HomeIcon, Calendar1Icon, LogOutIcon, LogInIcon } from "lucide-react";
 import Link from "next/link";
 import { quickSearchItems } from "@/app/_constants/search";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 const SidebarSheet = () => {
     return ( 
         
         <SheetContent className="overflow-y-auto">
-            <div className="py-5 border-solid border-b flex content-between gap-3">
-                <Avatar>
+            <SheetHeader>
+                <SheetTitle className="text-left">Menu</SheetTitle>
+            </SheetHeader>
+            
+            <div className="py-5 border-solid border-b flex justify-between gap-3">
+                <h2 className="font-bold">Olá, faça seu Login</h2>
+                <Dialog>
+                    <DialogTrigger asChild>
+                         <Button size="icon"> <LogInIcon > </LogInIcon> </Button>
+                    </DialogTrigger>
+
+                    <DialogContent className="w-[90%]">
+                        <DialogHeader>
+                            <DialogTitle>Faça Login na Plataforma</DialogTitle>
+                            <DialogDescription>
+                                Conecte-se usando sua conta do Google
+                            </DialogDescription>
+                        </DialogHeader>
+                        <Button variant="outline" className="w-full mt-4">
+                            <Image 
+                            src="/google.png" 
+                            alt="Fazer Login com Google"
+                            width={16}
+                            height={16} 
+                            />
+                            Continuar com Google
+                        </Button>
+                    </DialogContent>
+
+                </Dialog>              
+               {/* <Avatar>
                     <AvatarImage  src="https://images.unsplash.com/photo-1769096913011-5430ddef71ba?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU3fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D" />
                 </Avatar> 
                 <div> 
                     <p className="text-sm font-semibold">Estevan Gomes</p>
                     <p className="text-xs">gomes.estevan1@gmail.com</p>
-                </div>
+                </div>*/}
+                
             </div>
-            <SheetHeader>
-                <SheetTitle className="text-left">Menu</SheetTitle>
-            </SheetHeader>
+ 
+
             <div className="flex flex-col p-5 gap-4 border-b border-solid">
                 <SheetClose asChild >
                 <Button className="gap-2 justify-start" variant="ghost" >  
